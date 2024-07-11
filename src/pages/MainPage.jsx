fe8fd0abf3bfb42f768ce76282514ed8443a034c
Marcos Daniel Rusch <rusch.marcos@gmail.com>
2024-07-11 20:38:04 -0300

    Segundo Commit

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,10 +4,11 @@ import { Footer } from "../components/Footer";
 import "./MainPage.css";
 
 export const MainPage = ({ usuario, handleLogout }) => {
+  const backgroundImageUrl = `${import.meta.env.BASE_URL}assets/Racing-trainers-banner.webp`;
   return (
     <div className="MainPage">
       
-      <div className="TituloPaginaPrincipal">
+      <div className="TituloPaginaPrincipal" style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
         <h1>PAGINA PRINCIPAL</h1>
       </div>
       
